15f2038ff8c6ac4ae4779c54056249009952001c
cwadven <cwadven@naver.com>
2022-08-12 21:07:59 +0900

    chore: 이미지 업로드 3mb 이상 안됨
    - 이미지 업로드 3mb 이상 안됨

--- image_manage/serializers/imagemanage_serializers.py
+++ image_manage/serializers/imagemanage_serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import APIException
 
 from image_manage.models import BoardImage
 
@@ -7,3 +8,9 @@ class UploadImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = BoardImage
         fields = ("image",)
+
+    def validate_image(self, value):
+        size = value.size
+        if size and (size // 1024) > 3000:
+            raise APIException("이미지 최대 용량 3MB를 초과했습니다.")
+        return value
